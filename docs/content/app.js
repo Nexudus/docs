1a3172ee3b7a0a852db42ddef6fea4962a4705e2
Adrian <adrian@nexudus.com>
2025-03-25 16:35:04 +0000

    Move to docs.

--- docs/content/app.js
+++ docs/content/app.js
@@ -1,14 +1,14 @@
 function updateGifImages() {
-  const images = document.querySelectorAll('.step_frame');
+  const images = document.querySelectorAll(".step_frame");
   images.forEach(function (img) {
-    ['click', 'touchstart'].forEach(function (e) {
+    ["click", "touchstart"].forEach(function (e) {
       img.addEventListener(e, function () {
-        const gifSrc = img.getAttribute('data-gif');
-        const pngSrc = img.getAttribute('data-png');
+        const gifSrc = img.getAttribute("data-gif");
+        const pngSrc = img.getAttribute("data-png");
         img.src = gifSrc;
 
         function checkIfCollapsed() {
-          const openModals = document.querySelectorAll('dialog[open]');
+          const openModals = document.querySelectorAll("dialog[open]");
 
           if (openModals?.length == 0) img.src = pngSrc;
           else setTimeout(checkIfCollapsed, 250);
@@ -21,5 +21,4 @@ function updateGifImages() {
     });
   });
 }
-
 setTimeout(updateGifImages, 1000);
