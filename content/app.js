81168affbb3886f50efbdac5d06c6c6767834c51
Nexudus <adrian@nexudus.com>
2025-03-25 00:48:14 +0000

    Use 'click', 'touchstart' events

--- content/app.js
+++ content/app.js
@@ -1,21 +1,23 @@
 function updateGifImages() {
-  const images = document.querySelectorAll(".step_frame");
+  const images = document.querySelectorAll('.step_frame');
   images.forEach(function (img) {
-    img.addEventListener("click", function () {
-      const gifSrc = img.getAttribute("data-gif");
-      const pngSrc = img.getAttribute("data-png");
-      img.src = gifSrc;
+    ['click', 'touchstart'].forEach(function (e) {
+      img.addEventListener(e, function () {
+        const gifSrc = img.getAttribute('data-gif');
+        const pngSrc = img.getAttribute('data-png');
+        img.src = gifSrc;
 
-      function checkIfCollapsed() {
-        const openModals = document.querySelectorAll("dialog[open]");
+        function checkIfCollapsed() {
+          const openModals = document.querySelectorAll('dialog[open]');
 
-        if (openModals?.length == 0) img.src = pngSrc;
-        else setTimeout(checkIfCollapsed, 250);
-      }
+          if (openModals?.length == 0) img.src = pngSrc;
+          else setTimeout(checkIfCollapsed, 250);
+        }
 
-      setTimeout(checkIfCollapsed, 250);
+        setTimeout(checkIfCollapsed, 250);
 
-      return true;
+        return true;
+      });
     });
   });
 }
